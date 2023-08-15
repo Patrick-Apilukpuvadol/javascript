fc76c80926ee6efe59e4aafd2082f24a3519dc3b
Patrick-Apilukpuvadol <chimei983@hotmail.com>
2023-08-15 20:51:47 +1000

    added basic js to page

---
+++ script.js
@@ -0,0 +1,12 @@
+
+/**
+* This is a function.
+* @param {*} someArgument - Message to give to the function
+*/
+
+
+function someFunction(someArgument) {
+    console.log("This was given to the function:" = someArgument)
+}
+
+someFunction([1, 2, "banana"]);
